8ba6064ddbc22559aa7bca2cab7c41db249ff58a
GrubeB <33655111+GrubeB@users.noreply.github.com>
2022-08-13 14:03:11 +0200

    init

--- src/main/java/pl/dawid/main/castle/adapter/in/CreateCastleController.java
+++ src/main/java/pl/dawid/main/castle/adapter/in/CreateCastleController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.dawid.main.castle.application.CreateCastleService;
 import pl.dawid.main.castle.application.port.in.CreateCastleCommand;
 import pl.dawid.main.castle.application.port.in.CreateCastleUseCase;
 import pl.dawid.main.castle.domain.Castle;
-import pl.dawid.main.controller.CastleControllerImpl;
 
 @RestController
 @RequiredArgsConstructor
